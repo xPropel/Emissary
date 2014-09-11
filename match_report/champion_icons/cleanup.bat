07e67228b9ed3b6eee8d2b25526b7f6660a1c6f0
Darren Lin <lin.darren95@gmail.com>
2014-09-10 23:12:20 -0400

    Reorganized Directory + Home Page is Tournament Code

--- match_report/champion_icons/cleanup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET old=_Square_0.png
-set new=.png
-for /f "tokens=*" %%f in ('dir /b *.png') do (
-  SET newname=%%f
-  SET newname=!newname:%old%=%new%!
-  move "%%f" "!newname!"
-)
